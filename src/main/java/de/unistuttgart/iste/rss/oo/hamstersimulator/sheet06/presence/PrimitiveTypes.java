99874b05afcdaeeb83f05c951c7e4b20f6655628
Tim Panzer <tim.panzer1999@gmail.com>
2020-12-01 17:11:07 +0100

    added PrimitiveTypes class

---
+++ src/main/java/de/unistuttgart/iste/rss/oo/hamstersimulator/sheet06/presence/PrimitiveTypes.java
@@ -0,0 +1,29 @@
+package de.unistuttgart.iste.rss.oo.hamstersimulator.sheet06.presence;
+
+public class PrimitiveTypes {
+
+	public static void main(String[] args) {
+
+		byte maximumOfByte = 127;
+
+		int tenAsInteger = 10;
+		int fourAsInteger = 4;
+		double fourAsDouble = 4;
+
+		double rootOfTwo = Math.sqrt(2);
+		double zero = (rootOfTwo * rootOfTwo) - 2;
+
+		char characterA = 'a';
+		char characterB = 'b';
+
+		System.out.println(maximumOfByte + 1);
+		maximumOfByte++;
+		System.out.println(maximumOfByte);
+		System.out.println(tenAsInteger / fourAsInteger);
+		System.out.println(tenAsInteger / fourAsDouble);
+		System.out.println(zero);
+		System.out.println(characterB - characterA);
+
+	}
+
+}
